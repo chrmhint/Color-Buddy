f0b1353c9a252d2019bffc5b8384b67303e26a0c
LUKAS\lukas <lukas.saul.23@gmail.com>
2019-09-04 10:52:55 -0500

    Added wardrobes list

--- app/src/main/java/com/example/colorbuddy/wardrobeActivity.kt
+++ app/src/main/java/com/example/colorbuddy/wardrobeActivity.kt
@@ -3,13 +3,31 @@ package com.example.colorbuddy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.core.view.get
+import androidx.core.view.size
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_item.*
+import kotlinx.android.synthetic.main.activity_item.button
+import kotlinx.android.synthetic.main.activity_wardrobe.*
 
 class wardrobeActivity : AppCompatActivity() {
-
+    private lateinit var wardrobeList: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wardrobe)
+
+        wardrobeList = findViewById(R.id.wardrobeListView)
+        val wardrobes: Array<String> = arrayOf("Fancy","School","Casual","Gym")
+        val listItems = arrayOfNulls<String>(wardrobes.size)
+        for(i in 0 until wardrobes.size){
+            val wardrobe = wardrobes[i]
+            listItems[i] = wardrobe
+        }
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,listItems)
+        wardrobeList.adapter = adapter
+
         button.setOnClickListener{
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
